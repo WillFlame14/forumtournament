b5b97f08f12a5dd3bf9caa339bfec805ca7627c4
WillFlame14 <flameyanai@gmail.com>
2017-03-03 22:54:42 -0500

    Add files via upload

---
+++ Submissions.java
@@ -0,0 +1,15 @@
+package submissions;
+
+import java.time.*;
+
+public class Submissions {
+    
+    public static void main(String[] args) {
+        
+        Instant now = Instant.now();
+        System.out.println(System.currentTimeMillis());
+        System.out.println(now);
+        
+    }
+
+}
